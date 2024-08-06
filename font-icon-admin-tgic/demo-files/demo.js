41d847bac37ecfa978148a5dfa4b3161f95a4cca
Trương Công Lý <55310194+TCL47-GITHUB@users.noreply.github.com>
2024-08-06 09:18:03 +0700

    Add files via upload

--- font-icon-admin-tgic/demo-files/demo.js
+++ font-icon-admin-tgic/demo-files/demo.js
@@ -250,3 +250,49 @@ window.addEventListener("click", function (event) {
     popup.style.display = "none";
   }
 });
+
+
+// read iconData
+fetch('iconData.txt')
+    .then(response => response.text())
+    .then(data => {
+        // Xử lý dữ liệu từ file iconData.txt
+        const iconDataText = data.replace('export default', '');
+        const iconData = eval(iconDataText);
+
+        const container = document.getElementById('icon-container');
+
+        iconData.forEach(icon => {
+            const glyphDiv = document.createElement('div');
+            glyphDiv.classList.add('glyph', 'fs1');
+
+            const iconDiv = document.createElement('div');
+            iconDiv.classList.add('clearfix', 'bshadow0', 'pbs');
+
+            const iconElement = document.createElement('i');
+            icon.code.split(' ').forEach(cls => iconElement.classList.add(cls));
+            iconDiv.appendChild(iconElement);
+
+            const spanElement = document.createElement('span');
+            spanElement.classList.add('mls');
+            spanElement.textContent = icon.code;
+            iconDiv.appendChild(spanElement);
+
+            glyphDiv.appendChild(iconDiv);
+
+            const fieldset = document.createElement('fieldset');
+            fieldset.classList.add('fs0', 'size1of1', 'clearfix', 'hidden-false');
+
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.readOnly = true;
+            input.value = icon.content;
+            input.classList.add('unit', 'size1of2');
+            fieldset.appendChild(input);
+
+            glyphDiv.appendChild(fieldset);
+
+            container.appendChild(glyphDiv);
+        });
+    })
+    .catch(error => console.error('Error fetching iconData:', error));
