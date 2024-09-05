c51d0d8cbc16e5424da26f3be5ef2f7907fa0081
TCL47 <truongcongly.clt@gmail.com>
2024-09-05 16:50:41 +0700

    no message

--- user/main.js
+++ user/main.js
@@ -6,6 +6,12 @@ var prefix = "tgic ";
 let abc = "icon-";
 // Danh sách icon mới
 var newIcons = [
+  `${abc}solid-delivery-done`,
+  `${abc}solid-key`,
+  `${abc}solid-bookmark`,
+  `${abc}solid-chat`,
+  `${abc}microchip-1`,
+  `${abc}solid-microchip-1`,
   `${abc}logo-tgic`,
   `${abc}logo-linhkienx`,
   `${abc}logo-linhkienx-sm`,
